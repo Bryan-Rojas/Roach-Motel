a526a52d816d750054eeb3e6a5473b74464dded3
jopl93 <josephine@vestnet.dk>
2018-11-26 11:42:11 -0800

    RoomFactory class added.

---
+++ RoomFactory.java
@@ -0,0 +1,21 @@
+/**
+ * Created by josep on 26-11-2018.
+ */
+public class RoomFactory {
+
+    public Room createRoom(String type){
+
+        Room room = null;
+
+        if(type.equals("Regular")) {
+            room = new RegularRoom();
+        } else if(type.equals("Deluxe")) {
+            room = new DeluxeRoom();
+        } else if(type.equals("Suite")) {
+            room = new SuiteRoom();
+        }
+
+        return room;
+    }
+    
+}
