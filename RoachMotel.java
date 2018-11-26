fa9b735e6ac591106c12d13c46b9379eaeb0a48e
Bryan Rojas <bryroj14@gmail.com>
2018-11-26 11:25:07 -0800

    RoachMotel.java

--- RoachMotel.java
+++ RoachMotel.java
@@ -3,24 +3,42 @@ import java.util.Map;
 import java.util.Queue;
 
 public class RoachMotel {
-    private static RoachMotel RM = new RoachMotel();
+    private static RoachMotel RM;
     //private Map<Integer, Room> RM_map;
     private boolean noVacancySign;
     //private Queue<RoachColony>;
     
-    private RoachMotel(){
-        
-    }
+    private RoachMotel(){}
     
     public static RoachMotel getInstance(){
+        if(RM == null){
+            RM = new RoachMotel();
+        }
         return RM;
     }
     
-    /**
-     * Other methods protected by singleton-ness
-     */
-    protected static void demo(){
-        System.out.println("TESTING SINGLETON - IT WORKS");
+    public static String turnOnSign(){
+        /*
+        if(RM_map.size() == CAPACITY_SIZE){
+            return "Sorry, we're full. No Vacancy."
+        }
+        String welcome = "Welcome, we have " + (CAPACITY_SIZE - RM_map.size()) + " rooms available.";
+        return welcome;
+        */
+        return "placeholder, see psuedocode.";
     }
+    
+    /*
+    public static Room checkIn(RoachColony rc, String str, Amenity, am){
+        return Room object;
+    }
+    */
+    
+    /*
+    public static double checkOut(Room, int){
+        double cost = Room.calculateRate();
+        return cost;
+    }
+    */
     
 }
